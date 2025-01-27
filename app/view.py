7af0684d28e1d3869cce7a58276dbef6fdc349ae
TaibkKurbanaliev <65422168+TaibkKurbanaliev@users.noreply.github.com>
2025-01-27 23:37:50 +0400

    Add base template

--- app/view.py
+++ app/view.py
@@ -3,5 +3,4 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    user_name = "Inve"
-    return render_template("index.html", name = user_name)
+    return render_template("index.html")
