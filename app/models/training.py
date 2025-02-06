a2d5472f99fce64d0fbe12d03e3e99cebc2569a4
TaibkKurbanaliev <taibkurbanaliev@gmail.com>
2025-02-06 22:44:26 +0400

    Add create function and route to training

--- app/models/training.py
+++ app/models/training.py
@@ -3,5 +3,8 @@ from .. import db
 
 class Training(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    trainingName = db.Column(db.String(100))
+    muscleGroups = db.Column(db.String(100))
+    numberOfTimes = db.Column(db.Integer)
+    numberOfSets = db.Column(db.Integer)
     date = db.Column(db.DateTime, default=datetime.now())
--- app/routes/training.py
+++ app/routes/training.py
@@ -1,12 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, render_template, request
 from .. import db
 from ..models.training import Training
 
 training = Blueprint('training', __name__)
 
-@training.route('/training/<name>')
-def create_training(name):
-    training = Training(name=name)
-    db.session.add(training)
-    db.session.commit()
-    return "Training Created!"
+@training.route('/training/create', methods=['POST','GET'])
+def create():
+    if request.method == 'POST':
+        trainingName = request.form.get('trainingName')
+        muscleGroups = request.form.get('muscleGroups')
+        numberOfTimes = request.form.get('numberOfTimes')
+        numberOfSets = request.form.get('numberOfSets')
+        training = Training(trainingName=trainingName, muscleGroups=muscleGroups, numberOfTimes=numberOfTimes, numberOfSets=numberOfSets)
+        try:
+            db.session.add(training)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(str(e))
+        return redirect('/')
+    else:
+        return render_template('training/create.html')
